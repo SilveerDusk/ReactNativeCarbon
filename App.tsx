2a291e960cc0ba8ffe1336ddb9cc27592133294a
Jason <Jason1@jelincic.com>
2023-10-28 13:44:58 -0700

    feat: adds first two screens

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import SearchScreen from "./screens/SearchScreen";
-import HistoryScreen from "./screens/HistoryScreen";
-import CarbonDataType from "./types/CarbonDataType";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SearchScreen from './screens/SearchScreen';
+import HistoryScreen from './screens/HistoryScreen';
+import CarbonDataType from './types/CarbonDataType';
 
 // if you wanted to use a bottom tab navigator, you could!
 // you'd need to install yarn add @react-navigation/bottom-tabs
@@ -12,16 +12,19 @@ import CarbonDataType from "./types/CarbonDataType";
 
 // this is a type definition for the parameters that can be passed to the screens, using the navigation.navigate function
 export type NativeStackParamList = {
-    Search: undefined | { data: CarbonDataType[] };
-    History: undefined | { data: CarbonDataType[] };
+  Search: undefined | { data: CarbonDataType[] };
+  History: undefined | { data: CarbonDataType[] };
 };
 
 const NativeStackNavigator = createNativeStackNavigator<NativeStackParamList>();
 
 export default function App() {
-    return (
-        <NavigationContainer>
-            {/* TODO: How can we set up the screen routing using the native stack navigator defined above? */}
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <NativeStackNavigator.Navigator initialRouteName="History">
+        <NativeStackNavigator.Screen name="Search" component={SearchScreen} />
+        <NativeStackNavigator.Screen name="History" component={HistoryScreen} />
+      </NativeStackNavigator.Navigator>
+    </NavigationContainer>
+  );
 }
--- types/CarbonDataType.tsx
+++ types/CarbonDataType.tsx
@@ -3,5 +3,22 @@
 export default interface CarbonDataType {
     // TODO: define the structure of the response from the API
     // note: start here! the following line allows anything to be a part of the object, which isn't useful for us
-    [key: string]: any;
+    "url": "https://www.wholegraindigital.com/",
+    "green": true,
+    "bytes": 443854,
+    "cleanerThan": 0.83,
+    "statistics": {
+        "adjustedBytes": 335109.77,
+        "energy": 0.0005633320052642376,
+        "co2": {
+            "grid": {
+                "grams": 0.26758270250051286,
+                "litres": 0.14882949913078525
+            },
+            "renewable": {
+                "grams": 0.24250694721722435,
+                "litres": 0.13488236404222018
+            }
+        }
+    }
 }
